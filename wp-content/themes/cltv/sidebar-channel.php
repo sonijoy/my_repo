0122175e4e725bf11f70f1334ce5d860f1c43605
Josh Fester <joshfester@gmail.com>
2014-10-13 00:48:04 -0700

    updated event calendar link on channels

--- wp-content/themes/cltv/sidebar-channel.php
+++ wp-content/themes/cltv/sidebar-channel.php
@@ -94,7 +94,7 @@
 						<img src="<?php echo get_template_directory_uri(); ?>/images/pintrest.png" alt="pintrest" />
 					</a>
 				<?php endif; ?>
-				<a class="btn btn-primary btn-small" href="/events?author_id=<?php the_author_meta('ID'); ?>">Event Calendar</a>
+				<a class="btn btn-primary btn-small" href="/events/month/?author_id=<?php the_author_meta('ID'); ?>">Event Calendar</a>
 			</div>
 		</div>
 	</div>
